c13f071f8ff897548eb2551838b8a6ae582350b7
novan <novanxyz@gmail.com>
2018-03-23 16:42:14 +0700

    87% start to about

--- www/app/views/about.js
+++ www/app/views/about.js
@@ -1,6 +1,21 @@
 define(function(require){
-   var Base     = require('models/base');             
-   var tmpl     = require('text!templates/about.html');
-   var AboutView = Base.Page.extend({_name : 'about'});
+  var Base     = require('models/base');             
+  var Utils    = require('utils');             
+  var tmpl     = require('text!templates/about.html');
+  var AboutView = Base.Page.extend({
+    _name : 'about',
+    scan_license:function(){
+        var project= prompt("Enter your Vardion Project Id");        
+        if (!project) {
+            return Utils.toast('To use this app on production mode, you need to set up licensed Vardion server');
+        }
+        project = project.trim();
+        if (project){
+            console.log('downloading license for project:', project);
+        }
+    },
+   
+    
+    });
    return AboutView;
 })
